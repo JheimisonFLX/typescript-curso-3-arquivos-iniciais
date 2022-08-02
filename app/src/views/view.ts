d5013284b7a6ebbedb8bf45afa35ff5acdac197f
JheimisonFLX <jheimison_007@hotmail.com>
2022-08-02 09:18:25 -0400

    01 - Aula 09

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecução } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -14,7 +16,7 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-
+    @logarTempoDeExecução()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
